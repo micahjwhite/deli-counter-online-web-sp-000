386c78fd11a81d7bab7d8d55a9d52e81e9d52614
Micah White <micahjwhite@gmail.com>
2020-04-11 14:55:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,4 +23,9 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
 end
-    
+
+def now_serving(katz_deli)
+  if
+    x
+  else
+end
