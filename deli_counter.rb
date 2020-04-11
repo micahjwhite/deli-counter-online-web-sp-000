cdd1929d1700250013b38ab10a276385e91d798a
Micah White <micahjwhite@gmail.com>
2020-04-11 14:42:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,6 +18,9 @@ def take_a_number(katz_deli, name)
   if katz_deli.length == 0
     katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  elsif katz_deli.length >= 1
+    katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
 end
     
