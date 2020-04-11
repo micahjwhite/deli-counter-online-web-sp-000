642791b64e773ba00104221cf249ea224c84b434
Micah White <micahjwhite@gmail.com>
2020-04-11 14:29:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length >= 1
-    counter = 1
     new_array = []
+    counter = 1
     katz_deli.each do |name|
     new_array.push("#{counter}. #{name}")
     counter += 1
