e78423b2075457c87a5a781db04a883b3ec716e3
Micah White <micahjwhite@gmail.com>
2020-04-11 14:16:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,15 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  elsif katz_deli.length >= 1
+  if katz_deli.length >= 1
     counter = 1
-    katz_deli.each do |customer|
+    new_array = []
+    katz_deli.each do |name|
       puts "The line is currently: #{counter}. #{customer}"
       counter += 1
     end
+  elsif katz_deli.length == 0
+    puts "The line is currently empty."
   end
   line(katz_deli)
 end
