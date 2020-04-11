74eed8227f2a994e4edd51ce1f2b080a5b9e55f1
Micah White <micahjwhite@gmail.com>
2020-04-11 14:40:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,8 +17,7 @@ end
 def take_a_number(katz_deli, name)
   if katz_deli.length == 0
     katz_deli.push(name)
-    counter = 1
-    puts "Welcome, #{name}. You are number 1 in line.""
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line.
   end
 end
     
