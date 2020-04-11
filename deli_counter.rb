d61be9ef373080b30d778dbf2adcd203b724519a
Micah White <micahjwhite@gmail.com>
2020-04-11 14:07:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  elsif katz_deli.length >= 1
+    counter = 1
+    katz_deli.each do |customer|
+      puts "The line is currently: #{counter}. #{customer}"
+      counter += 1
+    end
+  end
+  line(katz_deli)
+end
+  
